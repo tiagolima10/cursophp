5c6af82dbfc0f3d79fb5fe5004986f6909554f97
Tiago Lima da Anunciação <tiagoanunc@gmail.com>
2024-09-01 17:04:38 -0300

    Desafio Cálculo de Idades

--- Desafios/Desafio 010 - Calculando Idade/index.php
+++ Desafios/Desafio 010 - Calculando Idade/index.php
@@ -8,38 +8,26 @@
 </head>
 <body>
     <?php 
-        $nota1 = $_GET['nota1'] ?? 1;
-        $nota2 = $_GET['nota2'] ?? 1;
-        $peso1 = $_GET['peso1'] ?? 1;
-        $peso2 = $_GET['peso2'] ?? 1;
-
-        $soma_pesos = $peso1 + $peso2;
-        $media_simples = ($nota1 + $nota2) / 2 ;
-        $media_ponderada = (($nota1 * $peso1) + ($nota2 * $peso2)) / $soma_pesos;
+        $nascimento = $_GET['nascimento'] ?? 0;
+        $ano = $_GET['ano'] ?? 0;
+        $idade_buscada = $ano - $nascimento;
     ?>
 
     <main>
         <h1>Informe um número:</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-            <label for="nota1">Nota 1:</label>
-            <input type="number" name="nota1" id="nota1" value="<?=$nota1?>" step="0.01" min="0" max="10">
-            <label for="peso1">Peso 1:</label>
-            <input type="number" name="peso1" id="peso1" value="<?=$peso1?>" step="0" min="0" max="5">
-            <label for="nota2">Nota 2:</label>
-            <input type="number" name="nota2" id="nota2" value="<?=$nota2?>" step="0.01" min="0" max="10">
-            <label for="peso2">Peso 2:</label>
-            <input type="number" name="peso2" id="peso2" value="<?=$peso2?>" step="0" min="0" max="5">
-            <input type="submit" value="Calcular Médias">
+            <label for="nascimento">Em que ano nasceu?</label>
+            <input type="number" name="nascimento" id="nascimento" value="<?=$nascimento?>" min="1900" max="<?php date('Y') ?>">
+            <label for="ano">Quer saber sua idade em que ano?</label>
+            <input type="number" name="ano" id="ano" value="<?=$ano?>" min="1900">
+            <input type="submit" value="Qual será minha idade?">
         </form>
     </main>
 
     <section>
-        <h2>Cálculo das Médias</h2>
+        <h2>Resultado</h2>
         <?php 
-            echo "<p><ul>
-                <li> Média Simples: " . number_format($media_simples, 2, ",", '.') . "</li>
-                <li> Média Ponderada: " . number_format($media_ponderada, 2, ",", '.') . " </li>
-            <ul>";
+            echo "<p>Quem nasceu em <strong>$nascimento</strong> terá <strong>$idade_buscada anos</strong> em $ano!</p>";
         ?>
     </section>
 </body>
