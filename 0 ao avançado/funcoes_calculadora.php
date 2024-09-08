474668648a3ee92405b7becdeb41470cffa3a8bc
Tiago Lima da Anunciação <tiagoanunc@gmail.com>
2024-09-08 20:25:14 -0300

    funções

---
+++ 0 ao avançado/funcoes_calculadora.php
@@ -0,0 +1,19 @@
+<?php 
+    function soma($a, $b) {
+        return $a + $b;
+    }
+
+    $resultado = soma(2, 5);
+    echo $resultado;
+
+    function subtracao($a, $b) {
+        return $a - $b;
+    }
+
+    function divisao($a, $b) {
+        return $a / $b;
+    }
+
+    function multiplicacao($a, $b) {
+        return $a * $b;
+    }
