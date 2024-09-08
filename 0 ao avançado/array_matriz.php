3fc0abc2d728877c72f845ffba7291dc87e7b275
Tiago Lima da Anunciação <tiagoanunc@gmail.com>
2024-09-08 20:39:29 -0300

    Matriz Array

---
+++ 0 ao avançado/array_matriz.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    $frutas = ['maçã', 'pera', 'uva'];
+    $frutas[] = 'melão'; // adiciona um novo elemento ao final do array
+    echo $frutas[1] . "<br>";
+    echo $frutas[2] . "<br>";
+    echo $frutas[3] . "<br>";
+
+    $pessoa = ['nome' => 'João', 'idade' => 25, 'cidade' => 'São Paulo'];
+    echo $pessoa['nome'] . "<br>";
+
+    $matriz = array(
+        array('a', 'b', 'c'),
+        array('d', 'e', 'f'),
+        array('g', 'h', 'i')
+      );
+
+    for ($x=0; $x<3; $x++) {
+        for ($y=0; $y<3; $y++) {
+            echo $matriz[$x][$y] . " ";
+        }
+        print "<br>";
+    }
+?>
+</body>
+</html>
