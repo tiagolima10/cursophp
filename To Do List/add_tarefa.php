c1f2316ab8b247242d35dad402dd053fa800adc3
Tiago Lima da Anunciação <tiagoanunc@gmail.com>
2024-09-09 21:04:41 -0300

    ToDo List PHP

---
+++ To Do List/add_tarefa.php
@@ -0,0 +1,23 @@
+<?php 
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $task = $S_POST['tarefa'];
+
+        // Conexão com o banco
+        $conn = new mysqli('localhost', 'root', '', 'todolist');
+
+        // Verifica se a conexão foi bem sucedida
+        if ($conn->connect_error) {
+            die("Conexão falhou: $conn->connect_error");
+        }
+
+        // Nova tarefa
+        $sql = "INSERT INTO tarefas (tarefa) VALUES ('$tarefa')";
+
+        if ($conn->query($sql) === TRUE) {
+            header('Location: index.php');
+        } else {
+            echo "Erro ao adicionar tarefa: $conn->error";
+        }
+
+        $conn->close();
+    }
---
+++ To Do List/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do List</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <h1>To-Do List PHP</h1>
+    <form action="add_tarefa" method="post">
+        <input type="text" name="tarefa"placeholder="Adicione uma nova tarefa" required>
+        <button type="submit">Adicionar</button>
+    </form>
+
+    <ul>
+        <?php
+            // Conexão com o banco
+            $conn = mysqli_connect('localhost','root','','todolist');
+
+            // Caso haja uma falha na conexão
+            if ($conn->connect_error) {
+                die("Conexão falhou $conn->connect_error");
+            }
+
+            // Varre o banco e seleciona todas as tarefas
+            $sql = "SELECT * FROM tarefas";
+            $result = $conn->query($sql);
+
+            // Se houver tarefas, roda o loop
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $checked = $row['completa'] ? 'checked' : '';
+                    echo "<li>
+                            <input type='checkbox' onclick='toggleComplete({$row['id']})' $checked>
+                            {$row['tarefa']}
+                            <a href='delete_tarefa.php?id={$row['id']}' class='delete'>Excluir</a>
+                        </li>";
+                }
+            } else {
+                echo "<li>Nenhuma tarefa encontrada</li>";
+            }
+
+            //  Fecha a conexão com o banco
+            $conn->close();
+        ?>
+    </ul>
+
+    <script>
+        function toggleComplete(id) {
+            window.location.href = `toggle_complete.php?id=${id}`;
+        }
+    </script>
+</body>
+</html>
