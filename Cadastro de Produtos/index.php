92114459ba40dae327db5d030e380d9e43b58624
Tiago Lima da Anunciação <tiagoanunc@gmail.com>
2024-09-12 19:57:33 -0300

    Adicionando Produtos

--- Cadastro de Produtos/index.php
+++ Cadastro de Produtos/index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sistema de Cadastro de Produtos</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <h1>Sistema de Cadastro de Produtos</h1>
+
+    <div class="menu">
+        <a href="./products/add_product.php" class="btn">Adicionar Novo Produto</a>
+        <a href="list_products.php" class="btn">Listar Produtos</a>
+    </div>
 </body>
 </html>
---
+++ Cadastro de Produtos/products/add_product.php
@@ -0,0 +1,40 @@
+<?= include '../includes/db_connect.php'; ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adicionar Produto</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <h1>Adicionar Produtos</h1>
+    <form action="add_product.php" method="POST">
+        <label for="nome">Nome do Produto: </label>
+        <input type="text" name="nome" id="nome" required>
+
+        <label for="descricao">Descrição: </label>
+        <textarea name="descricao" id="descricao" required></textarea>
+
+        <label for="preco">Preço:</label>
+        <input type="number" name="preco" id="preco" step="0.01" min="0" required>
+
+        <button type="submit">Adicionar</button>
+    </form>
+
+    <?php 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $nome = $_POST['nome'];
+            $descricao = $_POST['descricao'];
+            $preco = number_format($_POST['preco'], 2, ",", ".");
+
+            $sql = "INSERT INTO products (nome, descricao, preco) VALUES (?, ?, ?)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$nome, $descricao, $preco]);
+
+            echo "Produto adicionado com sucesso";
+        }
+    ?>
+</body>
+</html>
