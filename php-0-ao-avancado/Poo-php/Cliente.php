49db83ef6e0d3e2128dfb02bb05f763bb445ea6b
Tiago Lima da Anunciação <tiagoanunc@gmail.com>
2024-09-17 21:15:51 -0300

    Métodos Mágicos

---
+++ php-0-ao-avancado/Poo-php/Cliente.php
@@ -0,0 +1,25 @@
+<?php 
+
+class Cliente
+{
+    private $nome;
+
+    public function __construct($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function __toString()
+    {
+        return 'Nome do cliente: ' . $this->nome;
+    }
+
+    public function __invoke()
+    {
+        return 'Chamou como uma função';   
+    }
+}
+
+$cli1 = new Cliente('Elton');
+echo $cli1();
+// var_dump($cli1);
